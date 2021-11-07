dc563d83662ed81e24e725362b8bc4a9c977d956
Azzy Kesuma <official.azzy@gmail.com>
2021-11-07 10:53:05 +0700

    styling lightbox

--- app.js
+++ app.js
@@ -33,21 +33,62 @@ function showSlides(n) {
 
 const hamburger = document.querySelector('.hamburger');
 const aside = document.getElementById('aside');
+const main = document.querySelector('main');
+const sticky = document.querySelector('.sticky');
+const body = document.querySelector('body');
 
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('open');
   aside.classList.toggle('isOpen');
+  if(aside.classList.contains('isOpen')){
+    body.style.overflow = 'hidden';
+    main.style.opacity = 0.5;
+    sticky.style.display = 'none';
+  } else {
+    body.style.overflow = 'auto';
+    main.style.opacity = 1;
+    sticky.style.display = 'grid';
+  }
 });
 
 // aside nav function
 const hardwareNav = document.querySelector('.hardwareNav');
 const gamesNav = document.querySelector('.gamesNav');
 const game = document.getElementById('game');
+const hardware = document.getElementById('hardware');
 const gameCursorBar = document.getElementById('gameCursorBar');
+const hardwareCursorBar = document.getElementById('hardwareCursorBar');
 
 game.addEventListener('click', () => {
   gamesNav.classList.toggle('gamesNav__open');
   gameCursorBar.classList.toggle('gameCursorBar__open');
 });
+
+hardware.addEventListener('click', () => {
+  hardwareNav.classList.toggle('hardwareNav__open');
+  hardwareCursorBar.classList.toggle('hardwareCursorBar__open');
+});
+
+// lightbox
+const modalImage = document.querySelector('.modalImage');
+const gridWrap = document.querySelector('.gridWrap');
+const images = gridWrap.querySelectorAll('img');
+const close = document.getElementById('close-icon')
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    modalImage.classList.add('modalImage__open');
+
+    if(modalImage.classList.contains('modalImage__open')){
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'auto';
+    }
+  });
+});
+
+close.addEventListener('click', () => {
+  modalImage.classList.remove('modalImage__open')
+})
 
